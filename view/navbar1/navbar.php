507e4f5b8272889d8cf13a4eeb4f54573d077e53
Kevin Kevinsson <Kevin.kevin_256@hotmail.com>
2017-04-13 13:15:24 +0100

    navbar

---
+++ view/navbar1/navbar.php
@@ -0,0 +1,41 @@
+<?php
+$navbar = [
+    "config" => [
+        "navbar-class" => "nav"
+    ],
+    "items" => [
+        "hem" => [
+            "text" => "Hem",
+            "route" => "",
+            "aclass" => "navtext",
+        ],
+        "about" => [
+            "text" => "About",
+            "route" => "about",
+            "aclass" => "navtext",
+        ],
+        "report" => [
+            "text" => "Report",
+            "route" => "report",
+            "aclass" => "navtext",
+        ],
+    ]
+];
+
+foreach ($navbar as $key => $value) {
+    if ($key == "config") {
+        echo '<div class=' . $value["navbar-class"] . '>';
+        echo '<navbar>';
+    }
+
+
+    if ($key == "items") {
+        foreach ($value as $key1 => $navinfo) {
+            $url = $app->url->create($navinfo["route"]);
+            echo '<a class=' . $navinfo["aclass"] . ' href=' . "$url>" . $navinfo["text"] . "</a>";
+        }
+    }
+}
+echo "</navbar>";
+echo "</div>";
+echo "<body> \n <main>";
